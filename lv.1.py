9a24cb0ff8f609745edaafd9897aeace69a1fd6a
LukaGelitashvili <lukagelitashvili5@gmail.com>
2024-09-28 15:53:56 +0400

    first commit

---
+++ lv.1.py
@@ -0,0 +1,15 @@
+name = "luka"
+
+
+surname = "gelitashvili"
+
+# print(name)
+# print(surname)
+
+age = 13
+height = 180.34
+
+print(" i'm ")
+print(name + " " + surname)
+print("i'm 13 years old")
+print("and i'm nearly 6ft tall")
