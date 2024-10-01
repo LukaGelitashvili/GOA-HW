58b41e0a64a277ce3020e78fa4e4118857216caa
LukaGelitashvili <lukagelitashvili5@gmail.com>
2024-10-02 00:28:17 +0400

    commit

--- lv.1.py
+++ lv.1.py
@@ -1,15 +1,8 @@
-name = "luka"
+name =  "my name is luka"
+last_name = "gelitashvili,"
 
+age = "i'm 13 years old,"
+height = "i'm nearly 6ft tall,"
+country = "i'm from Georgia."
 
-surname = "gelitashvili"
-
-# print(name)
-# print(surname)
-
-age = 13
-height = 180.34
-
-print(" i'm ")
-print(name + " " + surname)
-print("i'm 13 years old")
-print("and i'm nearly 6ft tall")
+print(name + " " + last_name +" " + age + " " + height + " " + country)
