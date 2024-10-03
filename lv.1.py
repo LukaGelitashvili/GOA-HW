913d8799428291e4c39f21afe0af2d5c07f2a767
LukaGelitashvili <lukagelitashvili5@gmail.com>
2024-10-03 16:17:50 +0400

    Update lv.1.py

--- lv.1.py
+++ lv.1.py
@@ -2,7 +2,7 @@ name =  "my name is luka"
 last_name = "gelitashvili,"
 
 age = "i'm 13 years old,"
-height = "i'm nearly 6ft tall,"
+height = "i'm 6ft tall,"
 country = "i'm from Georgia."
 
 print(name + " " + last_name +" " + age + " " + height + " " + country)
