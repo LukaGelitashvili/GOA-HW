63a1deea615ecd6e8b6ac24e981e9929282e4a43
LukaGelitashvili <lukagelitashvili5@gmail.com>
2024-10-24 20:10:52 +0400

    first commit

--- Day 05/Homework/Lesson 0.py
+++ Day 05/Homework/Lesson 0.py
@@ -1,4 +1,38 @@
 num = 100
+num2 = 200
+num3 = 300
+num4 = 400
+
+print(num4 + num2)
+
+print(num4 - num2)
+
+print(num4 * num2)
+
+print(num4 / num2)
+
+print(num4 // num2)
+
+
+print(num + num2 + num3)
+
+print(num - num2 - num3)
+
+print(num * num2 * num3)
+
+print(num / num2 / num3)
+
+print(num // num2 // num3)
+
+print(num + num2)
+
+print(num - num2)
+
+print(num * num2)
+
+print(num / num2)
+
+print(num // num2)
 
 print(num + 100)
 
@@ -9,3 +43,203 @@ print(num * 100)
 print(num / 100)
 
 print(num // 100)
+
+print(100 + 100)
+
+print(100 - 100)
+
+print(100 * 100)
+
+print(100 / 100)
+
+print(100 // 100)
+
+print(200 + 100)
+
+print(200 - 100)
+
+print(200 * 100)
+
+print(200 / 100)
+
+print(200 // 100)
+
+print(300 + 100)
+
+print(300 - 100)
+
+print(300 * 100)
+
+print(300 / 100)
+
+print(300 // 100)
+
+print(400 + 100)
+
+print(400 - 100)
+
+print(400 * 100)
+
+print(400 / 100)
+
+print(400 // 100)
+
+print(500 + 200)
+
+print(500 - 200)
+
+print(500 * 200)
+
+print(500 / 200)
+
+print(500 // 200)
+
+print(600 + 200)
+
+print(600 - 200)
+
+print(600 * 200)
+
+print(600 / 200)
+
+print(600 // 200)
+
+print(700 + 200)
+
+print(700 - 200)
+
+print(700 * 200)
+
+print(700 / 200)
+
+print(700 // 200)
+
+print(800 + 200)
+
+print(800 - 200)
+
+print(800 * 200)
+
+print(800 / 200)
+
+print(800 // 200)
+
+print(900 + 200)
+
+print(900 - 200)
+
+print(900 * 200)
+
+print(900 / 200)
+
+print(900 // 200)
+
+print(1000 + 200)
+
+print(1000 - 200)
+
+print(1000 * 200)
+
+print(1000 / 200)
+
+print(1000 // 200)
+
+print(2000 + 200)
+
+print(2000 - 200)
+
+print(2000 * 200)
+
+print(2000 / 200)
+
+print(2000 // 200)
+
+print(3000 + 200)
+
+print(3000 - 200)
+
+print(3000 * 200)
+
+print(3000 / 200)
+
+print(3000 // 200)
+
+print(4000 + 200)
+
+print(4000 - 200)
+
+print(4000 * 200)
+
+print(4000 / 200)
+
+print(4000 // 200)
+
+print(5000 + 200)
+
+print(5000 - 200)
+
+print(5000 * 200)
+
+print(5000 / 200)
+
+print(5000 // 200)
+
+print(6000 + 200)
+
+print(6000 - 200)
+
+print(6000 * 200)
+
+print(6000 / 200)
+
+print(6000 // 200)
+
+print(7000 + 200)
+
+print(7000 - 200)
+
+print(7000 * 200)
+
+print(7000 / 200)
+
+print(7000 // 200)
+
+print(8000 + 200)
+
+print(8000 - 200)
+
+print(8000 * 200)
+
+print(8000 / 200)
+
+print(8000 // 200)
+
+print(9000 + 200)
+
+print(9000 - 200)
+
+print(9000 * 200)
+
+print(9000 / 200)
+
+print(9000 // 200)
+
+print(10000 + 200)
+
+print(10000 - 200)
+
+print(10000 * 200)
+
+print(10000 / 200)
+
+print(10000 // 200)
+
+print(20000 + 200)
+
+print(20000 - 200)
+
+print(20000 * 200)
+
+print(20000 / 200)
+
+print(20000 // 200)
