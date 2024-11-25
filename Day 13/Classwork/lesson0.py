61daeb900b70f29f8e60d44fa7d3480e8906c799
LukaGelitashvili <lukagelitashvili5@gmail.com>
2024-11-25 21:14:15 +0400

    first commit

---
+++ Day 13/Classwork/lesson0.py
@@ -0,0 +1,13 @@
+# Sequence: ნაბიჯების ლოგიკური თანმიმდევრობა, სადაც პროგრამა მიჰყვება ინსტრუქციებს ზედიზედ.
+# Iteration: ინსტრუქციების გამეორება ციკლის მეშვეობით.
+# Selection: გადაწყვეტილების მიღება, სადაც პროგრამა ირჩევს კონკრეტულ გზას. გამოიყენება პირობითი ოპერატორები, მაგალითად, if და else.
+
+
+temperature = 25
+
+if temperature > 30:
+    print("It's hot outside")
+elif temperature >= 20:
+    print("The weather is nice")
+else:
+    print("It's cold outside")
