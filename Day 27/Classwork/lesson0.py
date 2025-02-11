676bfed4a8d22f1c376d09bef6aa39164f089021
LukaGelitashvili <lukagelitashvili5@gmail.com>
2025-02-11 22:30:27 +0400

    first commit

--- Day 27/Classwork/lesson0.py
+++ Day 27/Classwork/lesson0.py
@@ -1,1 +1,22 @@
-# codewars იყო არ ვიცი ამ შემთხვევაში როგორ უნდა გავაკეთო აქ visual code studio-ში classwork
+# https://www.codewars.com/kata/50654ddff44f800200000004/train/python
+def multiply(a, b):
+    return a * b
+# https://www.codewars.com/kata/583710ccaa6717322c000105/train/python
+def simple_multiplication(number) :
+    if number % 2 == 0:
+        return number * 8
+    else:
+        return number * 9
+# https://www.codewars.com/kata/55cbd4ba903825f7970000f5/train/python
+def get_grade(s1, s2, s3):
+    n = (s1 + s2 + s3) / 3
+    if 90 <= n <= 100:
+        return "A"
+    elif 80 <= n < 90:
+        return "B"
+    elif 70 <= n < 80:
+        return "C"
+    elif 60 <= n < 70:
+        return "D"
+    elif 0<= n < 60:
+        return "F" 
--- Day 27/Homework/lesson0.py
+++ Day 27/Homework/lesson0.py
@@ -1,1 +1,38 @@
-# codewars-ის დავალებები არი და არ ვიცი ესენი როგორ გავაკეთო visual code studio-ში
+# https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/python
+a = "code"
+b = "wa.rs"
+name = a + b
+# https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/python
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+# https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/python
+def main (verb, noun):
+    return verb + noun
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+def even_or_odd(number):
+    if number % 2 ==0:
+        return "Even"
+    else:
+        return "Odd"
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/python
+def number_to_string(num):
+    return str(num)
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/python
+def boolean_to_string(b):
+    return str(b)
+# https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/python
+def say_hello(name):
+    return "Hello, " + name
+# https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/python
+def quarter_of(month):
+    if 0 < month <= 3:
+        return 1
+    elif 3 < month <= 6:
+        return 2
+    elif 6 < month <= 9:
+        return 3
+    elif 9 < month <= 12:
+        return 4 
