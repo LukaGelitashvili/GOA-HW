8cf9fb4114501554a7580dd72b7e8ce9f3806b1f
LukaGelitashvili <lukagelitashvili5@gmail.com>
2026-04-15 20:22:16 +0400

    first commit

---
+++ Day 13/Classwork/main.py
@@ -0,0 +1,89 @@
+# 1. python არის ერთერთი პროგრამირების ენა
+# 2. python პოპულარული იმიტომ არის რომ ის ადვილი შესასწავლი ენა არის და თანაც ადვილი წასაკითხია რომელიც ასევე გამოიყენება AI-ის შექმნაში ან რამე სხვა პროგრამის შესაქმნელად
+# 3. python-ის კოდი იწერება .py ფაილში
+# 4. terminal-ში გამოაქვს რასაც ჩაწერ შიგნით
+# 5. print("Hello, World!") 
+# 6. string ეწოდება ტექსტს python-ში 
+# 7. ""-ში იწერება string
+# 8. # name = "luka"
+    # print(name)
+
+    # first_name = "luka"
+    # last_name = "gelitashvili"
+    # full_name = first_name + " " + last_name
+    # print(full_name)
+    # print(name)
+    # print(last_name)
+    
+
+    #year = "2026"
+    #print(year)
+# 9. integer არის რიცხვები/ციფრები python-ში
+# 10. 
+    # num = 200
+    # print(num + 100)
+
+    # print(100 + 300)
+
+    # num = -100
+    # print(num * 30)
+# 11. string-სა და integer-ს ვერ მიუმატებ,გაამრავლებ,გამოაკლებ ან გაყოფ ერთმანეთზე
+# 12. print("python") terminal-ში გამოიტანს python-ს
+# 13. print(10) terminal-ში გამოიტანს 10-ს
+# 14. print("10") terminal-ში გამოიტანს 10-ს მარა string-ად
+# 15. ერთი string არის და მეორე კიდე integer არის და ერთმანეთს ვერ მიუმატებ ან გაამრავლებ
+# 16. variable არის ცვლადი რომელშიც რაღაც მონაცემებს ვინახავთ
+# 17. ცვლადს python-ში ვქმნით ასე: name = "luka" string ცვლადისთვის და num = 10 integer ცვლადისთვის
+# 18. name = "luka"
+    # age = 15
+    # city = "Tbilisi"
+# 19. name = "nika"
+    # print(name)
+# 20. age = 15
+    # print(age)
+# 21. "=" ნიშანი არის რაღაცის მინიჭების ოპერატორი python-ში
+# 22. პირველი ქმნის name string ცვლადს რომელშიც ინახება სახელი "luka" და მეორეს print(name) კიდე ეს ცვლადი ტერმინალში გამოაქვს
+# 23. ცვლადის სახელში რიცხვის გამოყენება შეიძლება მარა ის არ შეიძლება რომ იყოს დასაწყისში მაგალითად 1name = "luka" არ შეილება და name1 = "luka" შეილება
+# 24. b,c,d,e
+# 25. name,name1,last_name,age,age1,myAge
+# 26. input() ფუნქცია აძლევს მომხარებელს საშუალებას რო რაიმე ინფორმაცია შეიყვანოს შიგნით
+# 27. name  = input("Enter Your Name: ")
+# 28. name = input("Enter Your Name: ")
+    # print(name)
+# 29. ეს კოდი გამოიტანს მომხმარებლის შეყვანილ სახელს
+# 30. age = int(input("Enter Your Age: "))
+# 31. python-ში კომენტარი არის # ის მეორენაირად შეგილია დაწერო ctrl + ?-ით
+# 32. ერთ ხაზიანი კომენტარი იწერება ასე "#ფუქციები" 
+# 33.
+    # კოდის მაღლა
+    # print("hello world")
+    # # უბრალოდ
+    # print("") #კოდის გვერდით
+# 34. pritn არის არასწორი უნდა ეწეროს print
+# 35. ერთი " აკლია ("hello)- ში ის უნდა ყოფილიყო ("hello")
+# 36. luka-ს აკლია ""
+# 37. name = "luka"
+    # last_name = "gelitashvili"
+    # age = 15
+    # print(name)
+    # print(last_name)
+    # print(age)
+
+#38. 
+# name = "Gia"
+# surname = "Alikashvili"
+# age = 53
+# print(name + " " + surname + " " + age)
+
+# 39. case-sensitive ნიშნავს რომ თუ ცვლადი არის Name და შენ დაწერ print-ში name-ს იგი გამოვა როგორც error ანუ რომ ერთი და იგივე სიტყვა რომელიც სხვადასხვა ზომის ასოებიტაა დაწერილი სრულად სხვადასხვა რამეს ნიშნავს
+# 40. არა ეს არ არის ერთი და იგივე ცვლადი
+# 41. ტერმინალში გამოიტანს 5-ს
+# 42. ტერმინალში გამოიტანს 5-ს მარა სტრინგის სახით
+# 43. 5 ინტეგერია "5" სტრინგია
+# 44. fav_color = input("Enter Your Favorite Color: ")
+#     print(fav_color)
+# 45. name = input("Please Enter Your Name: ")
+#     print("hello," + name)
+
+name = input("Please Enter Your Name: ")
+print("Hello,", name)
